25a9b51751cf124ece36cf8aa24727fb856c2d39
Elias Dorneles <eliasdorneles@gmail.com>
2011-08-17 22:04:15 -0300

    * gnome-terminal pra padrão

--- launch-gtypist.sh
+++ launch-gtypist.sh
@@ -2,4 +2,9 @@
 
 export LANG=pt_BR
 export LC_ALL=pt_BR.ISO-8859-1
-xterm -geometry 72x34+100+40 -fn *-fixed-*-*-*-20-* -e "gtypist $*"
+
+# configure o terminal preferido
+#terminal="xterm -geometry 72x34+100+40 -fn *-fixed-*-*-*-20-*"
+terminal="gnome-terminal"
+
+$terminal -e "gtypist $*"
